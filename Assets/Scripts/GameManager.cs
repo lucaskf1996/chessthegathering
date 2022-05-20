6e5b662c4068b7e5ad97718f3cd99e2c83a36b34
lucaskf1996 <email@example.com>
2022-05-20 11:36:40 -0300

    preparing for card distribution

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager
 {
-    // Start is called before the first frame update
-    void Start()
+    private static GameManager _instance;
+    public enum GameState {WHITEPAWNS, BLACKPAWNS, WHITEHAND, BLACKHAND, WHITEMOVE, BLACKMOVE};
+    private GameObject WhiteHand, BlackHand;
+    public GameState gameState { get; private set; }
+
+    public static GameManager GetInstance()
     {
+        if(_instance == null)
+        {
+            _instance = new GameManager();
+        }
+        return _instance;
+    }
+
+    public void ChangeState(GameState nextState)
+    {
+        gameState = nextState;
+    }
+
+    private GameManager()
+    {
+        gameState = GameState.WHITEPAWNS;
+        WhiteHand = GameObject.Find("WhiteHand");
+        BlackHand = GameObject.Find("BlackHand");
+    }
+
+    public void PawnHand(int id){
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void RandomHand(int id){
+
     }
 }
