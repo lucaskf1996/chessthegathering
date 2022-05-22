f9c4b5ec983755bdcdd8405c2228efde6643e5bc
CEDipEngineering <cadudip@hotmail.com>
2022-05-22 09:09:14 -0300

    Implement Pawn

---
+++ Assets/Scripts/Pieces/Pawn.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+public class Pawn : Piece
+{
+    public int id {get; set; }
+    public Piece.STATE state {get; set; }
+    public List<int> legalMoves {get;}
+    public List<int> captureMoves {get;}
+    public int position {get; set; }
+    public Pawn(int id){
+        this.id = id;
+        this.state = Piece.STATE.DECK;
+        this.legalMoves = new List<int>();
+        this.captureMoves = new List<int>();
+        // If it's a black pawn
+        if (this.id) {
+            this.legalMoves.Add(8); // Go down
+            this.captureMoves.Add(7); // Diag left
+            this.captureMoves.Add(9); // Diag right
+        } else { // White pawn
+            this.legalMoves.Add(-8); // Go up
+            this.captureMoves.Add(-7); // Diag left
+            this.captureMoves.Add(-9); // Diag right
+        }
+        this.position = -1; // Not in play yet        
+    }
+
+    public void Capture(){
+        this.state = Piece.STATE.CAPTURED;
+        this.position = -1;
+    }
+}
